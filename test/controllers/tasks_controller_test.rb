a523a8eddb86559ebabd2658bb4aaad397e44a89
Hitin <phoenix-h@mail.ru>
2016-06-18 15:58:46 +0400

    Test with Factory Girl

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class TasksControllerTest < ActionController::TestCase
   def setup
     @task = create(:task)
-    @new_task = build(:task)
+    @attr = attributes_for(:task)
   end
 
   def test_index
-	get :index
-	assert_response :success
+	  get :index
+	  assert_response :success
   end
 
   def test_show
-	get :show, id: @task.id
-	assert_response :success
+	  get :show, id: @task.id
+	 assert_response :success
   end
 
   def test_new
-	get :new
-	assert_response :success
+	  get :new
+	  assert_response :success
   end
 
   def test_create
-	assert_difference('Task.count') do 
-	post :create, tasks: { title: @new_task.user, text: @new_task.email }
+	  assert_difference('Task.count') do 
+	  post :create, tasks: @attr
 	end
     assert_redirected_to task_path(assigns(:tasks))
   end
@@ -34,14 +34,14 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   def test_destroy 
-	assert_difference('Task.count', -1) do
-	delete :destroy, id: @task.id
-	end
+	  assert_difference('Task.count', -1) do
+	  delete :destroy, id: @task.id
+	  end
     assert_redirected_to tasks_path
   end
 
   def test_update
-	patch :update, id: @task.id, tasks: { title: @new_task.user, task: @new_task.email }
-	assert_redirected_to task_path(assigns(:tasks))
+	  patch :update, id: @task.id, tasks: @attr
+	  assert_redirected_to task_path(assigns(:tasks))
   end
 end
